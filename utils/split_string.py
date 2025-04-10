ac8d388c91ba35a3bdfea110b74ba7828eb2b75b
aditivn12 <aditivn@ad.unc.edu>
2025-04-10 12:00:02 -0400

    deleted two files

--- utils/split_string.py
+++
@@ -1,4 +0,0 @@
-def split_string(text: str, max_words: int = 100) -> list:
-    words = text.split()
-    chunks = [' '.join(words[i:i + max_words]) for i in range(0, len(words), max_words)]
-    return chunks
--- utils/uproot.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import faiss
-from typing import List
-def get_relevant_chunks(query_vec: np.ndarray, doc_embeddings: np.ndarray, doc_chunks: List[str], k: int = 3) -> List[str]:
-    dim = doc_embeddings.shape[1]
-    index = faiss.IndexFlatL2(dim)
-    index.add(np.array(doc_embeddings))
-    _, I = index.search(query_vec, k)
-    return [doc_chunks[i] for i in I[0]]
